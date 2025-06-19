2f7710f4fcfaed920258afbadb23bcb65eb6dc4e
Huyin311 <huyin311@gmail.com>
2025-06-20 03:32:49 +0700

    ket noi db firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCPvfv6HVUiumAL7L7sWnwmBk-hDLLU8ME",
+  authDomain: "dbinnercard.firebaseapp.com",
+  projectId: "dbinnercard",
+  storageBucket: "dbinnercard.firebasestorage.app",
+  messagingSenderId: "824788267602",
+  appId: "1:824788267602:android:f7ad43664e824d99a6258b",
+};
